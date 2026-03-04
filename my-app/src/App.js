9183c3599e17a33b32c199a22c7307b6037dec80
Terry <hiexpectations90@gmail.com>
2026-03-04 10:56:44 +0000

    나 스스로 오목 만드는중

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,66 @@
 import {useState} from 'react';
 import './App.css';
 
-function Board({value}) {
-  return <button className="myBoard">{value}</button>
+function Board({value, handleBoard}) {
+  return <button className="myBoard" onClick ={handleBoard}>{value}</button>
 }
 
-export default function buildBoard() {
+export default function BuildBoard() {
+  const [squares, setSquares] = useState(Array(10).fill(null))
+  const [oxTurn, setOxTurn] = useState(true)
+
+  function clickBoard(i) {
+    const newSquares = squares.slice();
+    if(newSquares[i])
+    {
+      return;
+    }
+    if(oxTurn)
+    {
+      newSquares[i] = "X";
+    }
+    else{
+      newSquares[i] = "O";
+    }
+    setOxTurn(!oxTurn);
+    setSquares(newSquares);
+  }
+
   return (
     <>
       <div>
-        <Board value='1' />
-        <Board value='2' />
-        <Board value='3' />
+        <Board value={squares[0]} handleBoard={() => clickBoard(0)} />
+        <Board value={squares[1]} handleBoard={() => clickBoard(1)} />
+        <Board value={squares[2]} handleBoard={() => clickBoard(2)} />
       </div>
       <div>
-        <Board value='4' />
-        <Board value='5' />
-        <Board value='6' />
+        <Board value={squares[3]} handleBoard={() => clickBoard(3)} />
+        <Board value={squares[4]} handleBoard={() => clickBoard(4)} />
+        <Board value={squares[5]} handleBoard={() => clickBoard(5)} />
       </div>
       <div>
-        <Board value='7' />
-        <Board value='8' />
-        <Board value='9' />
+        <Board value={squares[6]} handleBoard={() => clickBoard(6)} />
+        <Board value={squares[7]} handleBoard={() => clickBoard(7)} />
+        <Board value={squares[8]} handleBoard={() => clickBoard(8)}/>
       </div>
     </>
   );
+}
+
+function WhoWins({squares}) {
+  const endGame = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ]
+
+  for(let ax=0; ax<endGame.length(); ax++)
+  {
+    
+  }
 }
